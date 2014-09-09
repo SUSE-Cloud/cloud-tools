2fd69da1ddb64292f71cf34a182cd230d6618c10
Nanuk Krinner <nkrinner@suse.de>
2014-09-09 10:57:33 +0200

    cloud shutdown
    Add documentation and scrip snippets to shut down a cloud

---
+++ shutdown/shutdown_nodes.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Shutdown all nodes in a cluster
+# Warning: this eventually also shuts down the admin node
+
+for node in $(crowbar machines list); do
+  if [[ $node != *admin* ]] ; then
+    echo "Shutting down node #node"
+    ssh #node halt -p
+    if [[ $? == "0" ]];  then
+      echo "done"
+    else
+      echo "ERROR"
+    fi
+  fi
+done
