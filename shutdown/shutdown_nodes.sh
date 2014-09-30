9c9b6291a90f8f5dab92077fab5a29ff6d0ef2fc
Manuel Schnitzer <mschnitzer@suse.de>
2014-09-30 14:12:33 +0000

    Bugfix: The adminnode will stay online
    The program is now checking, if a node is having the "crowbar" role. If so,
    the node will stay online after executing the script.
    
    Minor changes: Output improving with colors for a better overview.

--- shutdown/shutdown_nodes.sh
+++ shutdown/shutdown_nodes.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 # Shutdown all nodes in a cluster
-# Warning: this eventually also shuts down the admin node
 
 for node in $(crowbar machines list); do
-  if [[ $node != *admin* ]] ; then
-    echo "Shutting down node $node"
-    ssh $node halt -p
-    if [[ $? == "0" ]];  then
-      echo "done"
-    else
-      echo "ERROR"
+    OUTPUT=`crowbar machines show $node roles | grep \"crowbar\"`
+    
+    if [ -z "$OUTPUT" ]; then
+        ssh $node halt -p
+        if [[ $? == "0" ]];  then
+            STATUS="$(tput setaf 2)done$(tput sgr0)"
+        else
+            STATUS="$(tput setaf 1)error$(tput sgr0)"
+        fi
+
+        echo "[$STATUS] Shutting down node $node"
     fi
-  fi
 done
